eb5ec777a5072954e2baa1d72f800a075c607220
Fahim Uddin <fahimuddin.dev@gmail.com>
2022-08-03 15:07:50 +0600

    Deploying on Heruko

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
 const userRouter = require("./routers/userRouter");
 const orderRouter = require("./routers/orderRouter");
 
 const app = express();
+
+app.use(compression());
 app.use(cors());
 app.use(express.json());
 

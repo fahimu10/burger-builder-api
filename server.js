86749af5feb81f7af535f9290131d9f6d315c0f7
Fahim Uddin <fahimuddin.dev@gmail.com>
2022-08-03 12:33:48 +0600

    Conncet to atlas

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = require("./app");
 const mongoose = require("mongoose");
+// Connet to localhost
+// const DB = process.env.LOCAL_MONGODB_SERVER;
+
+// Connect to MongoDB Atlas
+const DB = process.env.MONGODB_SERVER.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD
+);
 
 mongoose
-  .connect(process.env.MONGODB_SERVER)
+  .connect(DB)
   .then(() => {
     console.log("Connected to MongoDB");
   })
